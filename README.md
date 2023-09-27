36b8750d308928dd6fd135d0ee02f5f125e998f0
jelu <jenaehe@yahoo.com>
2023-09-27 17:35:53 -0600

    got the thing working, but my VS code won't let me preview it or install the preview

---
+++ README.md
@@ -0,0 +1,39 @@
+# Logomylogo 
+   
+ 
+ ## Table of Contents
+-[Description](#description)  
+-[Installation](#installation)  
+-[Usage](#usage)  
+-[Credits](#credits)  
+-[License](#license)  
+-[Badges](#badges)  
+-[Features](#features)  
+-[How to contribute](#contribute)  
+-[Tests](#tests)  
+
+ ## Description
+ 
+ I want to develop a simple logo for my projects.
+ To avoid paying a graphic designer.
+ It creates a simple logo
+ 
+ 
+ 
+ 
+
+## Installation
+
+## Usage
+
+## Credits
+
+## License
+undefined
+## Badges
+## Features
+
+## How to contribute
+
+## Tests
+
---
+++ index.js
@@ -0,0 +1,25 @@
+const { getUserInput } = require('./lib/user-input');
+const { saveSVGToFile } = require('./lib/file-utils');
+const LogoGenerator = require('./lib/logo-generator');
+
+async function main() {
+  try {
+    const userInput = await getUserInput();
+
+    const logoGenerator = new LogoGenerator(
+      userInput.text,
+      userInput.textColor,
+      userInput.shape,
+      userInput.shapeColor
+    );
+
+    const svgContent = logoGenerator.generate();
+    const fileName = 'logo.svg';
+
+    saveSVGToFile(fileName, svgContent);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+main();
